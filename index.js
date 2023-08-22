fe3908a79d7ae67348063d00f88cd08f47973194
surya <suryajoshi617@gmail.com>
2023-08-23 02:24:00 +0530

    change

--- index.js
+++ index.js
@@ -60,15 +60,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port:${PORT}`));
-
-    //*ADD DATA ONE TIME
-  //  User.insertMany(users);
-  //   Post.insertMany(posts)
+  
 
 
-  })
-  .catch((error) => {
-    console.log(`${error} did not connect`);
-  });
+
