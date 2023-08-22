a2993e17d615f08d4e6879db8c3082182226036c
surya <suryajoshi617@gmail.com>
2023-08-23 02:45:44 +0530

    change

--- index.js
+++ index.js
@@ -57,15 +57,19 @@ app.use("/posts",postRoutes);
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
+
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => {
-    console.log("Connected to MongoDB Atlas");
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port:${PORT}`));
+
+    //*ADD DATA ONE TIME
+  //  User.insertMany(users);
+  //   Post.insertMany(posts)
+
+
   })
   .catch((error) => {
-    console.error("Error connecting to MongoDB Atlas:", error);
+    console.log(`${error} did not connect`);
   });
-  
-
-
-
