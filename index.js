72b3a215366312997a79922a68ce08229c6eefb1
surya <suryajoshi617@gmail.com>
2023-08-23 02:34:17 +0530

    change

--- index.js
+++ index.js
@@ -59,7 +59,12 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  }).then(() => {
+    console.log("Connected to MongoDB Atlas");
   })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
   
 
 
